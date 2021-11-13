65231e968f164fb3b90a29f4cdb1347821b42058
sandeepdp <86524672+sandeepdp@users.noreply.github.com>
2021-11-13 09:46:05 +0530

    render list

---
+++ screens/Edit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Edit = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Edit
+
+const styles = StyleSheet.create({})
